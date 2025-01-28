2577b5ebdbb4d0bd01e47d446236f9f45fe3133c
Umut Tosun <umut781@icloud.com>
2025-01-28 19:45:44 +0300

    axios-review

--- reactjs/8.api-using/src/App.jsx
+++ reactjs/8.api-using/src/App.jsx
@@ -3,77 +3,54 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
+import { use } from "react";
 
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "http://localhost:3000";
 
 function App() {
+  //async ve await kullanimi api islemleri bittikten sonra asagidaki kod bloklari calismasi icin
   const getAllUsers = async () => {
     const response = await axios.get(BASE_URL + "/users");
-    console.log(response.data.id);
+    console.log(response.data);
   };
 
-  const getUserByIda = async (userId) => {
-    const response = await axios.get(BASE_URL + "/users/" + userId);
+  const getUserById = async (id) => {
+    const response = await axios.get(BASE_URL + "/users/" + id);
     console.log(response.data);
   };
 
   const createUser = async (newUser) => {
+    //Post veri eklemek icin kullanilir
     const response = await axios.post(`${BASE_URL}/users`, newUser);
-    //post veri eklemek icin kullanilir
-    console.log("response", response.data);
+    console.log(response);
   };
 
   const updateUser = async (userId, updatedUser) => {
-    // put veri guncellemek icin kulllanilir
+    // PUT : veri guncellemek icin kullanilir/
     await axios.put(`${BASE_URL}/users/${userId}`, updatedUser);
   };
 
-  const deletedUserById = async (userId) => {
-    const response = await axios.delete(`${BASE_URL}/users/${userId}`);
-    console.log(response.data);
-  };
-
-  const getUserById = async (userId) => {
-    const response = await axios.get(`${BASE_URL}/users/${userId}`);
-    return response.data.postId;
-  };
-
-  const getPostById = async (postId) => {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts/" + postId
-    );
-    return response.data;
-  };
-
-  const getPost = async () => {
-    var postId = await getUserById(1);
-    // const postdata = await getPostById(postId);
-    // console.log(postdata);
-    console.log(postId);
-    const postdata = await getPostById(7);
-    console.log(postdata);
+  const deleteUser = async (userId) => {
+    //Delete : veri silmek icin kullanilir
+    await axios.delete(`${BASE_URL}/users/${userId}`);
   };
   useEffect(() => {
     // getAllUsers();
-    // getUserById(3);
-    //--------------------------------
+    // getUserById(1);
+    // CREATE
     // const newUser = {
-    //   id: 4,
-    //   username: "bilal",
+    //   username: "diyar",
     //   password: "1234",
     // };
     // createUser(newUser);
-    //--------------------------------
-    // updateUser("4", {
-    //   username: "arhan",
-    //   password: "54213",
+    // UPDATE
+    // updateUser("6772", {
+    //   username: "deniz",
+    //   password: "12345",
     // });
-    //--------------------------------
-    // deletedUserById("4");
-
-    getPost();
+    // DELETE
+    // deleteUser("41ab");
   }, []);
-
   return <div></div>;
 }
 
