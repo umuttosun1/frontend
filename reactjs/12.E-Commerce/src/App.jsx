b28808a6f6b40cec9977b305a66471bd2fde0a07
Umut Tosun <umut781@icloud.com>
2025-01-30 19:36:02 +0300

    E-commerce 1/5

---
+++ reactjs/12.E-Commerce/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import PageContainer from "./container/PageContainer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <PageContainer>
+        <Header />
+      </PageContainer>
+    </>
+  );
+}
+
+export default App;
---
+++ reactjs/12.E-Commerce/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "../css/Header.css";
+import { BsFillBasket2Fill } from "react-icons/bs";
+import { IoMdSearch } from "react-icons/io";
+import { CiLight } from "react-icons/ci";
+import { FaRegMoon } from "react-icons/fa";
+
+function Header() {
+  const [theme, setTheme] = useState(false);
+  const changeTheme = () => {
+    const root = document.getElementById("root");
+    setTheme(!theme);
+    if (!theme) {
+      root.style.backgroundColor = "rgba(0, 0, 0, 0.86)";
+      root.style.color = "white";
+    } else {
+      root.style.backgroundColor = "white";
+      root.style.color = "black";
+    }
+    setTheme(!theme);
+  };
+  return (
+    <div className="header">
+      <div className="flex-row">
+        <img className="logo" src="./src/image/logo.jpeg" alt="" />
+        <p className="logo-text">U-Commerce</p>
+      </div>
+      <div className="flex-row">
+        <input className="input" type="text" placeholder="Search Something" />
+        <div>
+          <IoMdSearch className="icons" />
+          {theme ? (
+            <CiLight className="icons" onClick={changeTheme} />
+          ) : (
+            <FaRegMoon className="icons" onClick={changeTheme} />
+          )}
+          <BsFillBasket2Fill className="icons" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
