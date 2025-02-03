4841b8b75e28ae9eb71dc19bf2a7074a8a9b38d1
Umut Tosun <umut781@icloud.com>
2025-02-03 20:55:52 +0300

    typescript 1/4

---
+++ reactjs/13-Typescript-learning/src/Learning.ts
@@ -0,0 +1,10 @@
+// temel tipler :number , :string , :boolean ,
+// any degeri bilinmeyen degiskenler icin ilk degerini girmek icin kullanilir
+
+let age: number = 24;
+
+let name: string = "umut";
+let dsa: any = 23;
+
+console.log(age);
+console.log(name);
