cd65a02b71e1ff718bb8262d617c5bdaf0975fd4
Umut Tosun <umut781@icloud.com>
2025-02-04 16:43:35 +0300

    typescript todo 2/5

--- reactjs/14-Typescript-TodoApp/src/components/TodoCreate.tsx
+++ reactjs/14-Typescript-TodoApp/src/components/TodoCreate.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createTodo } from "../redux/todoSlice";
+import { TodoType } from "../types/types";
 
 function TodoCreate() {
+  const dispatch = useDispatch();
+
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  const handleCreateTodo = () => {
+    if (newTodo.trim().length == 0) {
+      alert("Todo giriniz!");
+      return;
+    }
+    const payload: TodoType = {
+      id: Math.floor(Math.random() * 99999),
+      content: newTodo,
+    };
+    dispatch(createTodo(payload));
+    setNewTodo("");
+  };
   return (
     <div className="todo-create">
-      <input placeholder="Create a todo" type="text" className="todo-input" />
-      <button className="todo-button">Create</button>
+      <input
+        value={newTodo}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setNewTodo(e.target.value)
+        }
+        placeholder="Create a todo"
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={handleCreateTodo} className="todo-button">
+        Create
+      </button>
     </div>
   );
 }
