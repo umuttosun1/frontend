2a4713179d019e41d092854a54db13a81609aae9
Umut Tosun <umut781@icloud.com>
2025-02-04 17:05:21 +0300

    typescript todo 4/5

--- reactjs/14-Typescript-TodoApp/src/components/Todo.tsx
+++ reactjs/14-Typescript-TodoApp/src/components/Todo.tsx
@@ -2,16 +2,26 @@ import React from "react";
 import { CiSquareRemove } from "react-icons/ci";
 import { FiEdit } from "react-icons/fi";
 import { TodoType } from "../types/Types";
+import { useDispatch } from "react-redux";
+import { removeTodoById } from "../redux/todoSlice";
 
 interface TodoProps {
   todoProps: TodoType;
 }
 function Todo({ todoProps }: TodoProps) {
+  const { id, content } = todoProps;
+
+  const dispatch = useDispatch();
+
+  const handleRemoveTodo = () => {
+    dispatch(removeTodoById(id));
+  };
+
   return (
     <div className="todo">
-      <div>{todoProps.content}</div>
+      <div>{content}</div>
       <div>
-        <CiSquareRemove className="icons" />
+        <CiSquareRemove onClick={handleRemoveTodo} className="icons" />
         <FiEdit className="icons" />
       </div>
     </div>
--- reactjs/14-Typescript-TodoApp/src/redux/todoSlice.tsx
+++ reactjs/14-Typescript-TodoApp/src/redux/todoSlice.tsx
@@ -13,9 +13,17 @@ export const todoSlice = createSlice({
     createTodo: (state: TodoInitialState, action: PayloadAction<TodoType>) => {
       state.todos = [...state.todos, action.payload];
     },
+    removeTodoById: (
+      state: TodoInitialState,
+      action: PayloadAction<number>
+    ) => {
+      state.todos = [
+        ...state.todos.filter((todo: TodoType) => todo.id !== action.payload),
+      ];
+    },
   },
 });
 
-export const { createTodo } = todoSlice.actions;
+export const { createTodo, removeTodoById } = todoSlice.actions;
 
 export default todoSlice.reducer;
