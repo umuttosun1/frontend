a990b8500d32df053101f2af80bc610527ed927e
Umut Tosun <umut781@icloud.com>
2025-02-05 17:47:54 +0300

    firebase 4/5

--- reactjs/18-firebase/src/components/Navbar.jsx
+++ reactjs/18-firebase/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { auth } from "../Firebase";
+import { Navigate, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const logout = async () => {
+    toast.success("Cikis yapiliyor");
+    setTimeout(() => {
+      signOut(auth);
+      navigate("/auth");
+    }, 2000);
+  };
   return (
     <div className="navbar">
       <div className="navbar-left"> FIREBASE</div>
-      <div className="navbar-right">Cikis Yap</div>
+      <div onClick={logout} className="navbar-right">
+        Cikis Yap
+      </div>
     </div>
   );
 }
--- reactjs/18-firebase/src/pages/Auth.jsx
+++ reactjs/18-firebase/src/pages/Auth.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import { FaGoogle } from "react-icons/fa";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import { toast } from "react-toastify";
 import { auth } from "../Firebase";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const register = async () => {
     try {
@@ -25,6 +30,19 @@ function Auth() {
       toast.error(error.message);
     }
   };
+
+  const login = async () => {
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      const user = response.user;
+      if (user) {
+        toast.success("Giris basarili");
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Giris yapilamadi " + error.message);
+    }
+  };
   return (
     <div className="auth">
       <h3 className="auth-header">Giris Yap / Kaydol</h3>
@@ -38,7 +56,7 @@ function Auth() {
         <input
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          type="text"
+          type="password"
           placeholder="Sifre"
         />
       </div>
@@ -47,7 +65,9 @@ function Auth() {
           <FaGoogle style={{ marginTop: "3px" }} />
           Google ile Giris
         </button>
-        <button className="btn-login">Giris Yap</button>
+        <button onClick={login} className="btn-login">
+          Giris Yap
+        </button>
         <button onClick={register} className="btn-register">
           Kayit ol
         </button>
