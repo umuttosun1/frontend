70ef4bcc81d0773fb9a2d137f636fe8422560fd2
Umut Tosun <umut781@icloud.com>
2025-02-09 20:21:37 +0300

    Mui Grid 2

--- reactjs/17-mui/src/components/MuiGrid.jsx
+++ reactjs/17-mui/src/components/MuiGrid.jsx
@@ -1,20 +1,38 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid2";
-import React from "react";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#1A2027",
+  }),
+}));
 
 function MuiGrid() {
-  //grid 12 parca olur
   return (
-    <div>
-      <Grid>
-        <Grid xs={12} sm={6} md={8} lg={10}>
-          item 1
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 6, md: 8 }}>
+          <Item>xs=6 md=8</Item>
         </Grid>
-        <Grid xs={6} sm={3} md={4} lg={5}>
-          item 2
+        <Grid size={{ xs: 6, md: 4 }}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+        <Grid size={{ xs: 6, md: 4 }}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+        <Grid size={{ xs: 6, md: 8 }}>
+          <Item>xs=6 md=8</Item>
         </Grid>
       </Grid>
-    </div>
+    </Box>
   );
 }
-
 export default MuiGrid;
